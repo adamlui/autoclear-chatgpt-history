6d172cd547abdb0dd86be4a3e1cc3e13ade9aec2
XiaoYingYo <759852125@qq.com>
2023-05-17 06:52:11 +0800

    update version number

--- greasemonkey/autoclear-chatgpt-history.user.js
+++ greasemonkey/autoclear-chatgpt-history.user.js
@@ -11,7 +11,7 @@
 // @name:es             Borrar Automáticamente el Historial de ChatGPT
 // @name:fr             Effacement Automatique de L'Historique ChatGPT
 // @name:it             Cancella Automaticamente Cronologia ChatGPT
-// @version             2023.5.16.3
+// @version             2023.5.17.1
 // @description         Auto-clears chat history when visiting chat.openai.com
 // @author              Adam Lui (刘展鹏), Tripp1e & Xiao-Ying Yo (小影哟)
 // @namespace           https://github.com/adamlui
